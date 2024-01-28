4a0ad379335ce4dc5820e497e38b467217b3dde4
Neil Warren <neil.p.warren@gmail.com>
2024-01-28 01:23:41 -0800

    Changed some set paths

--- lua/nelwarr/set.lua
+++ lua/nelwarr/set.lua
@@ -14,9 +14,11 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+local mac = vim.fn.has('macunix')
+local home = mac and "HOME" or "UserProfile"
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv(home) .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.incsearch = true
@@ -34,7 +36,11 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
-vim.g.android_sdk_path = vim.fn.has('windows') and "C:/Users/neilp/AppData/Local/Android/Sdk" or ""
-vim.g.python3_host_prog = vim.fn.has('windows') and "C:/Python312/python" or ""
+
+if mac == 1 then
+    vim.g.android_sdk_path = "C:/Users/neilp/AppData/Local/Android/Sdk"
+    vim.g.python3_host_prog = "C:/Python312/python"
+end
+
 vim.g.loaded_perl_provider = 0
 vim.g.unrealnvim_debug = true
